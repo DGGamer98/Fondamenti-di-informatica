1ea45399b4d9b9df49dc3e1a7ffdd3de166ff21c
DGGamer98 <davidegatta03@outlook.it>
2026-05-06 23:56:07 +0200

    Esercizi eccezzioni completati

--- Strutture di codice/Exception/esercizi/parcheggio.py
+++ Strutture di codice/Exception/esercizi/parcheggio.py
@@ -14,31 +14,51 @@ class PostoGiaLibero(ParcheggioException):
 class TargaInvalida(ParcheggioException):
     pass
 
-'''TODO Davide
 class Parcheggio:
-    def __init__(self, posti_totali):
-        # crea una lista di posti: [None, None, None, ...]
-        # None = libero, stringa = targa dell'auto
+    def __init__(self, posti_totale):
+        self.posti = [None] * posti_totale
 
     def parcheggia(self, numero_posto, targa):
-        # raise PostoNonEsistente se numero_posto fuori range
-        # raise TargaInvalida se targa è vuota o ha meno di 4 caratteri
-        # raise PostoOccupato se il posto non è None
-        # altrimenti parcheggia l'auto
-
+        if self.posti[numero_posto] is not None:
+            raise PostoOccupato("Il posto è già occupato!")
+        
+        if numero_posto >= len(self.posti):
+            raise PostoNonEsistente("Il parcheggio indicato non esiste")
+        else:
+            self.posti[numero_posto] = targa
+            print("[LOG] posto assegnato")
+            return
+        
     def libera(self, numero_posto):
-        # raise PostoNonEsistente se numero_posto fuori range
-        # raise PostoGiaLibero se il posto è già None
-        # altrimenti libera il posto
+        if numero_posto >= len(self.posti):
+            raise PostoNonEsistente("Il posto non esiste!")
+        
+        if self.posti[numero_posto] is None:
+            raise PostoGiaLibero("Il posto è già libero!")
+        
+        self.posti[numero_posto] = None 
+        print("[LOG] posto liberato")
 
     def cerca_auto(self, targa):
-        # cerca la targa in tutti i posti
-        # raise ValueError se non trovata
-        # restituisce il numero del posto
-
-    def mostra_parcheggio(self):
-        # stampa tutti i posti con stato libero/occupato
-'''
-
+        if targa not in self.posti:
+            raise ValueError ("Targa non trovata")
         
+        for auto in self.posti:
+            if targa == auto:
+                return self.posti.index(targa)
+
+    def stampa(self):
+        for x in self.posti:
+            print(x)
+        
+p = Parcheggio(3)
+p.parcheggia(2,"BA23CZX")
+p.stampa()
+
+print(p.cerca_auto("ZZZZ"))
+
+# p.libera(34)
+# p.stampa()
+
+
 
--- Strutture di codice/Exception/esercizi/squadra_di_calcio.py
+++ Strutture di codice/Exception/esercizi/squadra_di_calcio.py
@@ -1,24 +1,3 @@
-'''TODO Davide
-def aggiungi_giocatore(squadra, nome):
-    # gestisci ValueError se nome è una stringa vuota ""
-    # se il giocatore è già presente stampa "Giocatore già in squadra!"
-    # altrimenti aggiunge il nome alla lista
-
-def rimuovi_giocatore(squadra, indice):
-    # gestisci IndexError se l'indice non esiste
-    # altrimenti rimuove il giocatore con pop(indice)
-    # stampa chi è stato rimosso
-
-def cerca_giocatore(squadra, nome):
-    # gestisci ValueError se nome non è nella lista
-    # usa lista.index(nome) → lancia ValueError se non trovato
-    # restituisce l'indice dove si trova il giocatore
-
-def mostra_squadra(squadra):
-    # stampa tutti i giocatori con il loro indice
-    # es. "0 → Ronaldo"
-'''
-
 def aggiungi_giocatore(squadra, nome):
     if nome in squadra:
         print("Giocatore già presente")
