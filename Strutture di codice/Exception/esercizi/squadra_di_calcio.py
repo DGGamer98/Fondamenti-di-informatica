f3471ae280b45e26aa01780ff18639b75d83e3bf
DGGamer98 <davidegatta03@gmail.com>
2026-05-06 18:51:12 +0200

    esercizio squadra da calcio completato

--- Strutture di codice/Exception/esercizi/squadra_di_calcio.py
+++ Strutture di codice/Exception/esercizi/squadra_di_calcio.py
@@ -18,3 +18,56 @@ def mostra_squadra(squadra):
     # stampa tutti i giocatori con il loro indice
     # es. "0 → Ronaldo"
 '''
+
+def aggiungi_giocatore(squadra, nome):
+    if nome in squadra:
+        print("Giocatore già presente")
+        return
+    if nome == "":
+        raise ValueError ("Nome non può essere una stringa vuota")
+    else:
+        squadra.append(nome)
+        print("giocatore aggiunto")
+    
+        
+def rimuovi_giocatore(squadra, indice):
+    try:
+        squadra[indice]
+    except IndexError:
+        print("Sei uscito fuori dalla lista")
+    else:
+        squadra.pop(indice)
+        print("Giocatore rimosso")
+        
+def cerca_giocatore(squadra, nome):
+    try:
+        squadra.index(nome)
+    except ValueError:
+        print("giocatore non trovato")
+    else:
+        return squadra.index(nome)
+    
+def mostra_squadra(squadra):
+    counter = 0
+    for x in squadra:
+        counter+=1
+        print(f"{counter} -> {x}")
+        
+        
+        
+squadra = []
+
+# aggiungi giocatori
+aggiungi_giocatore(squadra, "Ronaldo")
+aggiungi_giocatore(squadra, "Messi")
+aggiungi_giocatore(squadra, "Mbappé")
+#aggiungi_giocatore(squadra, "")
+#aggiungi_giocatore(squadra, "Messi")
+
+
+#rimuovi_giocatore(squadra, 99)
+print(cerca_giocatore(squadra, "Messi"))
+
+mostra_squadra(squadra)
+
+        
