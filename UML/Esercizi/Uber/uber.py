6b30b2e63ba75ae11b76147200b2a902db8e9376
DGGamer98 <davidegatta03@gmail.com>
2026-06-17 16:05:27 +0200

    esercizi namespace + uml

---
+++ UML/Esercizi/Uber/uber.py
@@ -0,0 +1,12 @@
+'''Istruzioni
+Sviluppare un class diagram UML di un sistema tipo Uber facendo attenzione a classi, relazioni, attributi e loro molteplicità. Fornire un pdf o png, Di seguito un elenco incompleto di classi da prendere necessariamente in considerazione: 
+Utente 
+Passeggero 
+Guidatore 
+Veicolo (TipologiaPatente,max passeggeri, disabile, tariffa, lusso, Carburante, annoCostruzione, targa, colore) 
+Corsa 
+Indirizzo 
+Partenza 
+Destinazione 
+Pagamento 
+Recensione '''
